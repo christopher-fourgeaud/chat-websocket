cc04533d74d836d79b069629c42182364d0b0bbe
mounga <christopher.fourgeaud@gmail.com>
2017-09-15 13:52:57 +0200

    commit initial

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var bodyparser = require('body-parser');
+var socket = require('socket.io');
+var server = app.listen(3000);
+var io = socket(server);
+var ent = require('ent');
+var encode = require('ent/encode');
+var decode = require('ent/decode');
+
+app.use(express.static('public'));
+
+//Action sur la connection
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+        console.log(encode(msg));
+    })
+    console.log(encode('hello', { special: { l: true } }));
+    console.log(encode('hello', { special: { l: true } }));
+    console.log('User has connected');
+
+    //Action à la deconnection
+    socket.on('disconnect', function(){
+        console.log('User is disconnected !')
+    })
+})
